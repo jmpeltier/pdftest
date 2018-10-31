8a547fd22e98efa751d80720a5bc02114122bcec
James Peltier <>
2018-10-30 22:54:51 -0400

    Upgraded test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PDFProgressData, PDFDocumentProxy, PDFSource }  from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pdftestapp';
   pdfSrc: string = 'assets/pdftest.pdf';
+  pdf:any;
+  isLoaded:boolean=false;
+  numPages:number = 0;
+  showAllPages:boolean = false;
+  currentPage:number = 1;
+  outline: any[];
+
+  afterLoadComplete(pdf: PDFDocumentProxy) {
+    this.pdf = pdf;
+    this.numPages = pdf.numPages;
+    this.isLoaded = true;
+
+    this.pdf.getOutline().then((outline: any[]) => {
+      this.outline = outline;
+    });
+  }
+  incrementPage(){
+    this.currentPage +=1;
+  }
+
+  decrementPage(){
+    this.currentPage -=1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
@@ -11,7 +12,8 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
   ],
   imports: [
     BrowserModule,
-    PdfViewerModule
+    PdfViewerModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
